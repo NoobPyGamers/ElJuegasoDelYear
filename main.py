957d87075770b34bf1f29806def4e2e1aa0233cf
Erieru <eliel.buituron@gmail.com>
2019-12-05 09:44:55 -0300

    main updated

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-import pygame
+import pygame, sys
 from pygame.locals import *
+
+WIDTH=800
+HEIGHT=600
+
+def main():
+    screen=pygame.display.set_mode((WIDTH,HEIGHT))
+    pygame.display.set_caption("Terrible juego amigo")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit(0)
+    return 0
+
+if __name__ == '__main__':
+    pygame.init
+    main()
